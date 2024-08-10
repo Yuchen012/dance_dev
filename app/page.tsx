bf60e5e9a21e69b5186b67755817a3977f77721b
Yuchen012 <ncstarry0514k@gmail.com>
2024-08-10 21:25:01 +0900

    Adjust project structure for deployment

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Dance Studio Website</h1>
+      <p>This is a custom HTML content for the homepage.</p>
+    </div>
+  );
+}
