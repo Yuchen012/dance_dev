629662030a014ba6047f87bce9ea53e846c245b9
seki <102042736+HERUESTA@users.noreply.github.com>
2024-08-10 22:05:11 +0900

    Update page.tsx
    ダンスが好きです

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div>
       <h1>Welcome to My Dance Studio Website</h1>
       <p>This is a custom HTML content for the homepage.</p>
+      <∆p>I like dance!</Sp>
     </div>
   );
 }
